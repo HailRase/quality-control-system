5a114f611686369922cf62754eac8cb28c0e1cd5
HailRase <yurii_klimko@mail.ru>
2023-10-26 10:31:09 +0300

    added

--- src/s4-feature/f2-administrator/a6-supervisors/Supervisors.tsx
+++ src/s4-feature/f2-administrator/a6-supervisors/Supervisors.tsx
@@ -5,54 +5,50 @@ import {Button, Flex, Input, Layout, Space, Table} from "antd";
 import {Content, Header} from "antd/es/layout/layout";
 import {ColumnsType} from "antd/es/table";
 import {DeleteFilled, EditOutlined, KeyOutlined} from "@ant-design/icons";
+
 interface DataType {
-    key: React.Key;
-    fio: string;
-    login: number;
-    email: string;
+    key: React.Key
+    fio: string
+    login: string
+    email: string
 }
+
 const columns: ColumnsType<DataType> = [
     {
         title: 'ФИО',
         dataIndex: 'fio',
         key: 'fio',
         align: 'center',
-        render: () => <Input/>
+        render: (value, record, index) => <Input value={value} disabled/>
     },
     {
         title: 'Логин',
         dataIndex: 'login',
         key: 'login',
         align: 'center',
-        render: () => <Input/>
+        render: (value, record, index) => <Input value={value} disabled/>
     },
     {
         title: 'E-mail',
         dataIndex: 'email',
         key: 'email',
         align: 'center',
-        render: () => <Input/>
+        render: (value, record, index) => <Input value={value} disabled/>
     },
     {
         title: 'Обновить пароль',
-        dataIndex: 'refresh',
-        key: 'refresh',
         align: 'center',
-        render: () => <Button type={"primary"} ghost icon={<KeyOutlined />}/>
+        render: () => <Button type={"primary"} ghost icon={<KeyOutlined/>}/>
     },
     {
         title: 'Редактировать',
-        dataIndex: 'edit',
-        key: 'edit',
         align: 'center',
-        render: () => <Button type={"primary"} ghost icon={<EditOutlined />}/>
+        render: () => <Button type={"primary"} ghost icon={<EditOutlined/>}/>
     },
     {
         title: 'Удалить',
-        dataIndex: 'delete',
-        key: 'delete',
         align: 'center',
-        render: () => <Button danger ghost icon={<DeleteFilled />}/>
+        render: () => <Button danger ghost icon={<DeleteFilled/>}/>
     }
 ];
 const dataSource = [
@@ -61,117 +57,79 @@ const dataSource = [
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '2',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '3',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
+
     },
     {
         key: '4',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '5',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '6',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '7',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '8',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '9',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '10',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '11',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '12',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
     {
         key: '13',
         fio: '10 Downing Street',
         login: 'Mike',
         email: '10 Downing Street',
-        refresh: '',
-        edit: '',
-        delete: ''
     },
 ];
 const Supervisors = () => {
@@ -183,7 +141,7 @@ const Supervisors = () => {
                         Управление учётными записями супервизоров
                     </Header>
                     <Content className={s.dictionariesLayoutItemContent}>
-                        <Table style={{marginTop:'15px'}} columns={columns} dataSource={dataSource}/>
+                        <Table style={{marginTop: '15px'}} columns={columns} dataSource={dataSource}/>
                     </Content>
                 </Layout>
                 <Layout className={s.dictionariesLayoutItem}>
@@ -196,10 +154,11 @@ const Supervisors = () => {
                             <Input placeholder={'Введите логин пользователя...'}/>
                             <Input placeholder={'Введите почтовый адрес пользователя...'}/>
                             <Input placeholder={'Введите пароль пользователя...'}/>
-                            <Button type={"default"} style={{color: '#797575', borderColor: '#797575', borderRadius: '3px'}}>
+                            <Button type={"default"}
+                                    style={{color: '#797575', borderColor: '#797575', borderRadius: '3px'}}>
                                 Отменить
                             </Button>
-                            <Button type={"primary"} ghost style={{borderRadius: '3px'}} >Добавить</Button>
+                            <Button type={"primary"} ghost style={{borderRadius: '3px'}}>Добавить</Button>
                         </Flex>
                     </Content>
                 </Layout>
